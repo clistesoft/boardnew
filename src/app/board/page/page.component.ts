d48caac5e64de7465308bacc6893ae33fb21d7bc
clistesoft <sachin@clistesoft.com>
2021-02-07 15:45:19 +0530

    toolbox working

--- src/app/board/page/page.component.ts
+++ src/app/board/page/page.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
 export class PageComponent implements OnInit {
+  public keyPressed: any;
+  public permanentMode: any;
 
-  constructor() { }
+  constructor() {
+    // default permanentMode is selection
+    this.permanentMode = 'selection';
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  @HostListener('window:keyup', ['$event'])
+
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.keyPressed = event.code;
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
+
+  addItem(value:any){
+    this.permanentMode = value;
+  }
 
   ngOnInit(): void {
   }
--- src/app/board/page/toolbox/toolbox.component.ts
+++ src/app/board/page/toolbox/toolbox.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'toolbox',
   templateUrl: './toolbox.component.html',
   styleUrls: ['./toolbox.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ToolboxComponent implements OnInit {
+export class ToolboxComponent implements OnInit, OnChanges {
+  public activeButton: any;
 
-  constructor() { }
+  constructor() {}
+  @Input()
+  mode!: string;
+  @Output() toolSelectedEvent = new EventEmitter<string>();
 
-  ngOnInit(): void {
+  onToolSelect(value: string) {
+    this.toolSelectedEvent.emit(value);
   }
 
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    // Add '${implements OnChanges}' to the class.
+    console.log('changed in menu', this.mode);
+  }
 }
